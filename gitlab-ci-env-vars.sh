e140acc577193c5411a0a11295687b64b0d23cec
Andy Savage <andy@savage.hk>
2021-05-31 17:37:50 +0800

    Fixed up project namespace issues

--- gitlab-ci-env-vars.sh
+++ gitlab-ci-env-vars.sh
@@ -16,6 +16,7 @@ random() {
 #export CI_DEPLOY_FREEZE=${CI_DEPLOY_FREEZE:-"true"}
 #export CI_COMMIT_REF_PROTECTED=${CI_COMMIT_REF_PROTECTED:-"true"}
 [ ! -z "$CI_DEPLOY_FREEZE" ] && export CI_DEPLOY_USER=${CI_DEPLOY_USER:-"$(git log -1 --pretty=format:'%an')"}
+export CI_PROJECT_REPOSITORY_LANGUAGES=${CI_PROJECT_REPOSITORY_LANGUAGES:-"ruby,javascript,html,css"}
 export CI_COMMIT_TIMESTAMP=${CI_COMMIT_TIMESTAMP:-"$(git log -1 --format="%cd" --date="iso8601-strict")"}
 export CI_COMMIT_MESSAGE=${CI_COMMIT_MESSAGE:-"$(git log -1 --pretty=%B)"}
 [ "$(echo "$CI_COMMIT_MESSAGE" | wc -c)" -gt 1000 ] && export CI_COMMIT_DESCRIPTION=${CI_COMMIT_DESCRIPTION:-"$(echo "$CI_COMMIT_MESSAGE" | tail -n+2)"}  || export CI_COMMIT_DESCRIPTION=${CI_COMMIT_DESCRIPTION:-"$CI_COMMIT_MESSAGE"}
@@ -26,19 +27,19 @@ export CI_COMMIT_SHA=${CI_COMMIT_SHA:-"$(git rev-parse HEAD)"}
 export CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA:-"$(git rev-parse --short HEAD)"}
 export CI_COMMIT_BEFORE_SHA=${CI_COMMIT_BEFORE_SHA:-"$(git rev-parse ${CI_COMMIT_SHA}^1)"}
 export CI_COMMIT_REF_NAME=${CI_COMMIT_REF_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}
-export CI_COMMIT_REF_SLUG=${CI_COMMIT_REF_SLUG:-"$(echo "$CI_COMMIT_REF_NAME" | sed 's|/|-|g')"}
+export CI_COMMIT_REF_SLUG=${CI_COMMIT_REF_SLUG:-"$(echo "$CI_COMMIT_REF_NAME" | tr '[[:upper:]]' '[[:lower:]]' | sed 's|[^a-z0-9]|-|g')"}
 export CI_COMMIT_TAG=${CI_COMMIT_TAG:-"$(git tag --points-at HEAD)"}
 export CI_PROJECT_URL=${CI_PROJECT_URL:-"$(git remote get-url origin --push | sed -r 's:git@([^/]+)\:(.*\.git):https\://\1/\2:g' | sed 's|\.git$||')"}
 export CI_REPOSITORY_URL=${CI_REPOSITORY_URL:-"$(git remote get-url origin --push | sed -r 's:git@([^/]+)\:(.*\.git):https\://\1/\2:g')"}
 export CI_REGISTRY=${CI_REGISTRY:-"registry.$(echo "$CI_REPOSITORY_URL" | awk -F[/:] '{print $4}')"}
 export CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE:-"${CI_PROJECT_NAMESPACE}"}
 export CI_PROJECT_ROOT_NAMESPACE=${CI_PROJECT_ROOT_NAMESPACE:-"$(echo "$CI_PROJECT_URL" | cut -d'/' -f4)"}
-export CI_PROJECT_NAMESPACE=${CI_PROJECT_NAMESPACE:-"$(echo "$CI_PROJECT_URL" | cut -d'/' -f5-)"}
+export CI_PROJECT_NAMESPACE=${CI_PROJECT_NAMESPACE:-"$(echo "$CI_PROJECT_URL" | sed 's|http[s]*://\(.*\)/.*$|\1|' | cut -f2- -d'/')"}
 export CI_PROJECT_NAME=${CI_PROJECT_NAME:-"$(echo "$CI_PROJECT_URL" | sed 's#.*/##')"}
 export CI_PROJECT_PATH=${CI_PROJECT_PATH:-"${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"}
-export CI_PROJECT_PATH_SLUG=${CI_PROJECT_PATH_SLUG:-"$(echo "$CI_PROJECT_PATH" | sed 's|/|-|g')"}
+export CI_PROJECT_PATH_SLUG=${CI_PROJECT_PATH_SLUG:-"$(echo "$CI_PROJECT_PATH" | tr '[[:upper:]]' '[[:lower:]]' | sed 's|[^a-z0-9]|-|g')"}
 export CI_PAGES_DOMAIN=${CI_PAGES_DOMAIN:-"gitlab.io"}
-export CI_PAGES_URL=${CI_PAGES_URL:-"https://${CI_PROJECT_ROOT_NAMESPACE}.${CI_PAGES_DOMAIN}/${CI_PROJECT_NAMESPACE#"$CI_PROJECT_ROOT_NAMESPACE"}/${CI_PROJECT_NAME}"}
+export CI_PAGES_URL=${CI_PAGES_URL:-"https://${CI_PROJECT_ROOT_NAMESPACE}.${CI_PAGES_DOMAIN}/${CI_PROJECT_NAMESPACE#"$CI_PROJECT_ROOT_NAMESPACE/"}/${CI_PROJECT_NAME}"}
 export CI_PROJECT_ID=${CI_PROJECT_ID:-"34"}
 export CI_JOB_ID=${CI_JOB_ID:-"$(random 100)"}
 export CI_RUNNER_ID=${CI_RUNNER_ID:-"$(random 10)"}
@@ -62,7 +63,7 @@ export CI_SERVER_VERSION_PATCH=${CI_SERVER_VERSION_PATCH:-"0"}
 export CI_SERVER_VERSION=${CI_SERVER_VERSION:-"${CI_SERVER_VERSION_MAJOR}.${CI_SERVER_VERSION_MINOR}.${CI_SERVER_VERSION_PATCH}"}
 export CI_API_V4_URL=${CI_API_V4_URL:-"${CI_SERVER_URL}/api/v4"}
 export CI_ENVIRONMENT_NAME=${CI_ENVIRONMENT_NAME:-""}
-export CI_ENVIRONMENT_SLUG=${CI_ENVIRONMENT_SLUG:-"$(echo "$CI_ENVIRONMENT_NAME" | sed 's|/|-|g')"}
+export CI_ENVIRONMENT_SLUG=${CI_ENVIRONMENT_SLUG:-"$(echo "$CI_ENVIRONMENT_NAME" | tr '[[:upper:]]' '[[:lower:]]' | sed 's|[^a-z0-9]|-|g')"}
 export CI_ENVIRONMENT_URL=${CI_ENVIRONMENT_URL:-""}
 export CI_REGISTRY_USER=${CI_REGISTRY_USER:-""}
 
